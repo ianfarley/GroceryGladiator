b3222b3bf7f30534f165ba0701ee7a8f0c1890e8
Leon Lu <37962135+OmegaSylux@users.noreply.github.com>
2021-03-15 17:38:53 -0400

    Implemented clickable ingredients if player is near

--- GroceryGladiator/Assets/Scripts/System/CollectibleBehavior.cs
+++ GroceryGladiator/Assets/Scripts/System/CollectibleBehavior.cs
@@ -8,20 +8,41 @@ public class CollectibleBehavior : MonoBehaviour
     public string collectibleName;
     public Image smallListCross; 
     public Image largeListCross;
-
+    //private bool canPick;
+    public GameObject playerObject;
+    private float pickupRange = 1.75f;
     void Start()
     {
+       // this.canPick = false;
         this.collectibleName = this.gameObject.name;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+    /*
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.name == "Pickup_Range")
+        {
+            this.canPick = true;
+        }
     }
 
-    void OnCollisionEnter (Collision col)
+    void OnTriggerExit(Collider col)
     {
-        if(col.gameObject.tag == "Player")
+        if (col.gameObject.name == "Pickup_Range")
+        {
+            this.canPick = false;
+        }
+    }
+    */
+    void OnMouseDown()
+    {
+        Debug.Log(this.gameObject.name + " has been clicked!");
+        if (Vector3.Distance(playerObject.transform.position, this.gameObject.transform.position) < pickupRange)
         {
             //Notify level manager of collectible collected
             LevelManager.UpdateLevelCollectibles(this.collectibleName);
@@ -33,5 +54,24 @@ public class CollectibleBehavior : MonoBehaviour
             Debug.Log("Destroying object named: " + collectibleName);
             Destroy(this.gameObject);
         }
+        /*
+        if (canPick)
+        {
+            //Notify level manager of collectible collected
+            LevelManager.UpdateLevelCollectibles(this.collectibleName);
+            //Signal UI to update
+            smallListCross.gameObject.SetActive(true);
+            largeListCross.gameObject.SetActive(true);
+
+            //Destroy object
+            Debug.Log("Destroying object named: " + collectibleName);
+            Destroy(this.gameObject);
+        }
+
+        else if (!canPick)
+        {
+            Debug.Log(gameObject.name + " cannot be picked up. Player is too far away.");
+        }
+        */
     }
 }
