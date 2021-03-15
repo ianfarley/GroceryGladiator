730c98b5c951e4858293305fbf29b25e57524d67
SNSi1025x <71395876+SNSi1025x@users.noreply.github.com>
2021-03-14 23:51:24 -0400

    Re-added JJ with AI and made Granny faster

--- GroceryGladiator/Assets/Scripts/NPC/PatrolAgent.cs
+++ GroceryGladiator/Assets/Scripts/NPC/PatrolAgent.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class GrannyPatrolAgent : MonoBehaviour
+public class PatrolAgent : MonoBehaviour
 {
     [SerializeField]
     private Transform[] points;
