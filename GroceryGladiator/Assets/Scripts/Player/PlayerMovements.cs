07dd094732a90b83fedc590f5b3f0dcf2d20464b
Leon Lu <37962135+OmegaSylux@users.noreply.github.com>
2021-03-15 15:38:38 -0400

    Added animation to the player character

--- GroceryGladiator/Assets/Scripts/Player/PlayerMovements.cs
+++ GroceryGladiator/Assets/Scripts/Player/PlayerMovements.cs
@@ -8,11 +8,13 @@ public class PlayerMovements : MonoBehaviour
     public float playerMoveSpeed = 130.0f;
     public float playerRotationSpeed = 50.0f;
     public static bool canMove;
+    [SerializeField] private Animator anim;
 
     void Start()
     {
         canMove = true;
         playerRig = gameObject.GetComponent<Rigidbody>();
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,31 +25,41 @@ public class PlayerMovements : MonoBehaviour
             //Move the player forward
             if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
             {
+                anim.SetBool("isMoving", true);
                 playerRig.AddForce(transform.forward * (playerMoveSpeed * Time.fixedDeltaTime));
             }
 
             //Move the player backward
             if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
             {
+                //anim.SetBool("isMoving", true);
                 playerRig.AddForce(-transform.forward * (playerMoveSpeed * Time.fixedDeltaTime));
             }
 
             //Turn 90 to the character's right
             if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
             {
+                //anim.SetBool("isMoving", true);
                 transform.Rotate(Vector3.up * (playerRotationSpeed * Time.fixedDeltaTime));
             }
 
             //Turn 90 to the character's left
             if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
             {
+               //anim.SetBool("isMoving", true);
                 transform.Rotate(Vector3.down * (playerRotationSpeed * Time.fixedDeltaTime));
+            }
+
+            else if(!Input.anyKey)
+            {
+                anim.SetBool("isMoving", false);
             }
         }
 
         else if (canMove == false)
         {
             //Player cannot move!
+            //anim.SetBool("isMoving", false);
         }
 
     }
